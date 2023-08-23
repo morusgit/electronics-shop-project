7230a27791e4fb9f4cddf49aa56565ee87c179ad
Maxim Verbitskiy <morus@bk.ru>
2023-08-24 00:50:52 +0300

    Add pyTests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,21 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+"""Пишем тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item():
+    """Создаём тестовый экземпляр класса"""
+    return Item("Тестовый товар", 10.0, 5)
+
+
+def test_calculate_total_price(item):
+    """Проверка вывода стоимости товаров"""
+    assert item.calculate_total_price() == 50.0
+
+
+def test_apply_discount(item):
+    """Проверка скидки"""
+    item.pay_rate = 0.8
+    item.apply_discount()
+    assert item.price == 8.0
